fb0bd4275191adccbe933e1a24524e66ad1dc1f0
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2023-05-01 16:56:44 +0530

    Update DemoApplication.java

--- src/main/java/com/nuvepro/demo/DemoApplication.java
+++ src/main/java/com/nuvepro/demo/DemoApplication.java
@@ -2,12 +2,12 @@ package com.nuvepro.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -19,10 +19,6 @@ public class DemoApplication {
 	{
 		return "index";
 	}
-	@GetMapping("/status")
-	public String getHomePage()
-	{
-		return 200;
-	}
+	
 
 }
